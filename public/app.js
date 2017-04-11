9bdf264a070d0b048dfbd9094c95854bfb25fa41
kimnorth <benignhumour@gmail.com>
2017-04-11 22:10:14 +0100

    Added a button that takes the user to Tokyo and displays an info window.

--- public/app.js
+++ public/app.js
@@ -2,8 +2,20 @@ var initialise = function(){
   var container = document.getElementById('main-map');
   var center = {lat: 51.451169, lng: 0.052639};
   var mainMap = new MapWrapper(container, center, 10);
+  var tokyoButton = document.getElementById('tokyo-button');
   mainMap.addClickEvent();
   mainMap.addInfoWindow(center, "Welcome to Sunny Eltham!")
+
+  var tokyo = function(){
+    var tokyo = {lat: 35.689487, lng: 139.691706};
+    mainMap.googleMap.setCenter(tokyo);
+    mainMap.addInfoWindow(tokyo, "Tokyo");
+  }
+
+  tokyoButton.onclick = tokyo;
+
 }
+
+
 
 window.onload = initialise;
